c28f19435e28118d4881d4c1c59e379d3e5c79d8
CharlesVIO <charlesviossat6@gmail.com>
2025-01-14 14:44:31 +0100

    Ajout de setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='ASCII_Generator',
+ version='1.0',
+ author='CharlesVIO',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
